c4ac3f9d8fc9964f95fc49d5c17a802b5b0b8beb
David Bazile <dbazile@radiantblue.com>
2016-11-05 19:21:21 -0400

    Increase test coverage of scenes service

--- test/service/test_scenes.py
+++ test/service/test_scenes.py
@@ -12,18 +12,19 @@
 # specific language governing permissions and limitations under the License.
 
 import logging
-import unittest
+import unittest.mock
 
 import requests_mock as rm
+from requests import ConnectionError
 
 from test import helpers
 
 from bfapi.db import DatabaseError
-from bfapi.service import scenes as service
+from bfapi.service import scenes
 
 
 @rm.Mocker()
-class SceneGetTest(unittest.TestCase):
+class GetSceneTest(unittest.TestCase):
     def setUp(self):
         self._mockdb = helpers.mock_database()
         self._logger = logging.getLogger('bfapi.service.scenes')
@@ -35,41 +36,41 @@ class SceneGetTest(unittest.TestCase):
 
     def test_calls_correct_url(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        service.get('landsat:LC80110632016220LGN00')
+        scenes.get('landsat:LC80110632016220LGN00')
         self.assertEqual('https://pzsvc-image-catalog.localhost/image/landsat:LC80110632016220LGN00',
                          m.request_history[0].url)
 
     def test_fetches_scenes(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
-        self.assertIsInstance(scene, service.Scene)
+        scene = scenes.get('landsat:LC80110632016220LGN00')
+        self.assertIsInstance(scene, scenes.Scene)
 
     def test_yields_correct_capture_date(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertEqual('2016-08-07T15:33:42.572449+00:00',
                          scene.capture_date.isoformat())
 
     def test_yields_correct_bands(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertSetEqual({'blue', 'cirrus', 'coastal', 'green', 'nir', 'panchromatic', 'red', 'swir1', 'swir2',
                              'tirs1', 'tirs2'},
                             set(scene.bands.keys()))
 
     def test_yields_correct_cloud_cover(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertEqual(1.47, scene.cloud_cover)
 
     def test_yields_correct_id(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertEqual('landsat:LC80110632016220LGN00', scene.id)
 
     def test_yields_correct_geometry(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertIsInstance(scene.geometry, dict)
         self.assertIsInstance(scene.geometry.get('coordinates'), list)
         self.assertEqual([[[-81.5725334896228, -3.29632263428811], [-79.934112104967, -3.64597257671337],
@@ -79,45 +80,96 @@ class SceneGetTest(unittest.TestCase):
 
     def test_yields_correct_resolution(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertEqual(30, scene.resolution)
 
     def test_yields_correct_sensor_name(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertEqual('Landsat8', scene.sensor_name)
 
     def test_yields_correct_uri(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        scene = service.get('landsat:LC80110632016220LGN00')
+        scene = scenes.get('landsat:LC80110632016220LGN00')
         self.assertEqual('https://pzsvc-image-catalog.localhost/image/landsat:LC80110632016220LGN00',
                          scene.uri)
 
-    def test_handles_catalog_http_errors_gracefully(self, m: rm.Mocker):
+    def test_throws_if_catalog_is_unreachable(self, _):
+        with unittest.mock.patch('requests.get') as stub:
+            stub.side_effect = ConnectionError()
+            with self.assertRaises(scenes.CatalogError):
+                scenes.get('landsat:LC80110632016220LGN00')
+
+    def test_gracefully_handles_catalog_http_errors(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', status_code=500, text='oh noes')
-        with self.assertRaises(service.CatalogError):
-            service.get('landsat:LC80110632016220LGN00')
+        with self.assertRaises(scenes.CatalogError):
+            scenes.get('landsat:LC80110632016220LGN00')
 
     def test_throws_if_scene_does_not_exist(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', status_code=404, text=RESPONSE_SCENE_NOT_FOUND)
-        with self.assertRaises(service.NotFound):
-            service.get('landsat:LC80110632016220LGN00')
+        with self.assertRaises(scenes.NotFound):
+            scenes.get('landsat:LC80110632016220LGN00')
+
+    def test_throws_if_scene_id_is_malformed(self, _):
+        with self.assertRaises(scenes.MalformedSceneID):
+            scenes.get('lolwut')
 
     def test_saves_scene_to_database(self, m: rm.Mocker):
         m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-        service.get('landsat:LC80110632016220LGN00')
+        scenes.get('landsat:LC80110632016220LGN00')
         self.assertTrue(self._mockdb.executed)
 
-    def test_handles_database_errors_gracefully(self, m: rm.Mocker):
+    def test_gracefully_handles_database_errors(self, m: rm.Mocker):
+        m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
         self._mockdb.raise_on_execute()
         with self.assertRaises(DatabaseError):
-            m.get('/image/landsat:LC80110632016220LGN00', text=RESPONSE_SCENE)
-            service.get('landsat:LC80110632016220LGN00')
+            scenes.get('landsat:LC80110632016220LGN00')
+
+
+@rm.Mocker()
+@unittest.mock.patch('bfapi.piazza.to_auth_header', return_value='Basic Og==')
+class GetEventTypeIDTest(unittest.TestCase):
+    def setUp(self):
+        self._logger = logging.getLogger('bfapi.service.scenes')
+        self._logger.disabled = True
+
+    def tearDown(self):
+        self._logger.disabled = False
+
+    def test_calls_correct_url(self, m: rm.Mocker, _):
+        m.get('/eventTypeID', text=RESPONSE_EVENT_TYPE_ID)
+        scenes.get_event_type_id()
+        self.assertEqual(
+            'https://pzsvc-image-catalog.localhost/eventTypeID?pzGateway=https%3A%2F%2Fpz-gateway.localhost',
+            m.request_history[0].url,
+        )
+
+    def test_passes_correct_headers(self, m: rm.Mocker, _):
+        m.get('/eventTypeID', text=RESPONSE_EVENT_TYPE_ID)
+        scenes.get_event_type_id()
+        self.assertEqual('Basic Og==', m.request_history[0].headers['Authorization'])
+
+    def test_returns_event_type_id(self, m: rm.Mocker, _):
+        m.get('/eventTypeID', text=RESPONSE_EVENT_TYPE_ID)
+        event_type_id = scenes.get_event_type_id()
+        self.assertEqual(RESPONSE_EVENT_TYPE_ID, event_type_id)
+
+    def test_gracefully_handles_http_errors(self, m: rm.Mocker, _):
+        m.get('/eventTypeID', status_code=500, text='oh noes')
+        with self.assertRaises(scenes.CatalogError):
+            scenes.get_event_type_id()
+
+    def test_throws_if_event_type_id_is_malformed(self, m: rm.Mocker, _):
+        m.get('/eventTypeID', text='lolwut')
+        with self.assertRaises(scenes.CatalogError):
+            scenes.get_event_type_id()
 
 
 #
 # Fixtures
 #
+
+RESPONSE_EVENT_TYPE_ID = 'ffffffff-ffff-ffff-ffff-ffffffffffff'
 
 RESPONSE_SCENE_NOT_FOUND = (
     'Unable to retrieve metadata for landsat:LC80110632016220LGN00: redis: nil even using wildcard search'
