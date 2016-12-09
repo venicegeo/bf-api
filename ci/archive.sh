5e95367ad31b5145ee65c67f2b26640c1a59cdde
David Bazile <dbazile@radiantblue.com>
2016-12-09 10:54:11 -0500

    CI: Fix virtualenv’s Python path (third times the charm)

--- ci/archive.sh
+++ ci/archive.sh
@@ -8,15 +8,14 @@ source $root/ci/vars.sh
 
 ## Install Dependencies ########################################################
 
+# HACK -- workaround for Python 3.5 in Jenkins
+if [ $JENKINS_HOME ]; then
+    . /opt/rh/rh-python35/enable
+fi
+
 # Create or enter virtual environment
 if [ ! -f .env/bin/activate ]; then
-    if [ $(uname) == "Darwin" ]; then
-        virtualenv --python=python3.5 .env
-    else
-        # HACK -- workaround for Python 3.5 in Jenkins
-        . /opt/rh/rh-python35/enable
-        virtualenv --python=python35 .env
-    fi
+    virtualenv --python=python3.5 .env
 fi
 . .env/bin/activate
 
