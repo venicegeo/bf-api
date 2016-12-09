f00b660bd2ce6591147e7c758dcba3d1057dfbd6
David Bazile <dbazile@radiantblue.com>
2016-12-09 16:27:51 -0500

    CI: Request API key on deployment

---
+++ ci/cf_push_stage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -ex
+
+./ci/_cf_push.sh
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,5 +3,4 @@
 cd $(dirname $(dirname $0))  # Return to root
 
 echo "Cleaning up"
-rm -rfv bf-api.zip vendor report .coverage
-
+rm -rfv bf-api.zip vendor report .coverage manifest.{dev,int,stage,prod}.yml
