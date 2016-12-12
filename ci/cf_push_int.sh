3b32096fbea232f37e7bd789b3f40871b1f59f63
David Bazile <dbazile@radiantblue.com>
2016-12-12 10:17:51 -0500

    CI: Prevent legacy catalog artifacts from breaking int

--- ci/cf_push_int.sh
+++ ci/cf_push_int.sh
@@ -4,3 +4,8 @@ export PIAZZA_URL=https://piazza.int.geointservices.io/key
 export MANIFEST_FILENAME=manifest.int.yml
 
 ./ci/_cf_push.sh
+
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
+# FIXME - Remove after moving away from legacy image catalog harvest workflow
+echo "    SKIP_PRODUCTLINE_INSTALL: 1"
+# HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK HACK
