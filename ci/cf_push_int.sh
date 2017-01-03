cfdf55d15abe06d6f9cc8ee742c20305add1f7df
David Bazile <dbazile@radiantblue.com>
2017-01-03 12:36:50 -0500

    CI: Re-point int to stage

--- ci/cf_push_int.sh
+++ ci/cf_push_int.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 
-export PIAZZA_URL=https://piazza.int.geointservices.io/v2/key
+export PIAZZA_URL=https://piazza.stage.geointservices.io/v2/key
 export MANIFEST_FILENAME=manifest.int.yml
 
 ./ci/_cf_push.sh
