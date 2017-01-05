794c437f2f5c5e0f398506a055ef760ca3515544
bbarden <benjamin.m.barden@gmail.com>
2017-01-05 10:25:22 -0500

    quick sql fix

--- sql/schema.install.sql
+++ sql/schema.install.sql
@@ -64,6 +64,7 @@ CREATE TABLE __beachfront__job_user (
     PRIMARY KEY (job_id, user_id),
     FOREIGN KEY (job_id) REFERENCES __beachfront__job(job_id) ON DELETE CASCADE
 --    FOREIGN KEY (user_id) REFERENCES __beachfront__user(geoaxis_uid) ON DELETE CASCADE
+--       FIXME: above line can be enabled when end-to-end GeoAxis flow is complete
 );
 
 CREATE TABLE __beachfront__job_error (
