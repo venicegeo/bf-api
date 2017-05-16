7cfa0284b01eb6dd8b0446f64d5e4b4c08565ae3
Patrick D <patrick.doody@rgi-corp.com>
2017-05-16 07:58:02 -0400

    Debug

--- bfapi/middleware.py
+++ bfapi/middleware.py
@@ -45,7 +45,8 @@ def apply_default_response_headers(response: flask.Response) -> flask.Response:
 
 def strip_cookie_headers(response: flask.Response) -> flask.Response:
     # do not extend life of Flask session on automated status checks
-    print(flask.request.script_root)
+    log = logging.getLogger(__name__)
+    log.debug(flask.request.script_root)
     if flask.request.method == 'GET' and flask.request.script_root in ENDPOINTS_DO_NOT_EXTEND_SESSION:
         response.headers.remove('Set-Cookie')
     return response
