c8b098e6c9b5be6ae639a6518e32445172dbafb9
Patrick D <go0ty@users.noreply.github.com>
2017-05-16 08:44:26 -0400

    Fix Path Syntax

--- bfapi/middleware.py
+++ bfapi/middleware.py
@@ -45,7 +45,7 @@ def apply_default_response_headers(response: flask.Response) -> flask.Response:
 
 def strip_cookie_headers(response: flask.Response) -> flask.Response:
     # do not extend life of Flask session on automated status checks
-    if flask.request.method == 'GET' and flask.request.script_root in ENDPOINTS_DO_NOT_EXTEND_SESSION:
+    if flask.request.method == 'GET' and flask.request.path in ENDPOINTS_DO_NOT_EXTEND_SESSION:
         response.headers.remove('Set-Cookie')
     return response
 
