396310f9e2909e8f4728c4d360bfa87a0f62488b
Filip Sufitchi <filips@imagemattersllc.com>
2017-06-01 16:42:00 -0400

    21450 fetch java, liquibase, postgresql-jdbc in archive script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,4 +3,4 @@
 cd $(dirname $(dirname $0))  # Return to root
 
 echo "Cleaning up"
-rm -rfv bf-api.zip vendor report .coverage manifest.{dev,int,stage,prod}.yml
+rm -rfv bf-api.zip vendor migrations/vendor report .coverage manifest.{dev,int,stage,prod}.yml
