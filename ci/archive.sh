96c95896558d5dc3d249743257ecaf177899777a
Sonny Saniev <ssaniev@gmail.com>
2017-09-14 14:06:01 -0400

    removing wget from archive.sh

--- ci/archive.sh
+++ ci/archive.sh
@@ -22,17 +22,17 @@ mkdir -p vendor
 mkdir -p migrations/vendor
 
 # Liquibase 3.2.2
-wget https://repo1.maven.org/maven2/org/liquibase/liquibase-core/3.2.2/liquibase-core-3.2.2.jar \
-  -O migrations/vendor/liquibase.jar
+#wget https://repo1.maven.org/maven2/org/liquibase/liquibase-core/3.2.2/liquibase-core-3.2.2.jar \
+#  -O migrations/vendor/liquibase.jar
 
 # PostgreSQL JDBC 42.0.0
 
-wget https://jdbc.postgresql.org/download/postgresql-42.0.0.jre6.jar \
-  -O migrations/vendor/postgresql.jar
+#wget https://jdbc.postgresql.org/download/postgresql-42.0.0.jre6.jar \
+#  -O migrations/vendor/postgresql.jar
 
 # Java 8 u131
-wget http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220305_d54c1d3a095b4ff2b6607d096fa80163 \
-  -O - | \
+#wget http://javadl.oracle.com/webapps/download/AutoDL?BundleId=220305_d54c1d3a095b4ff2b6607d096fa80163 \
+#  -O - | \
   tar xz --directory migrations/vendor
 
 if [ -d migrations/vendor/jre ]; then
