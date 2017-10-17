48501f984e1ef9e215ab20e15242b661467efd8a
Filip Sufitchi <filips@imagemattersllc.com>
2017-10-17 16:54:55 -0400

    Modify test mock environment configuration

--- src/test/java/org/venice/beachfront/bfapi/MockApiConfig.java
+++ src/test/java/org/venice/beachfront/bfapi/MockApiConfig.java
@@ -7,15 +7,15 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.venice.beachfront.bfapi.model.Environment;
 import org.venice.beachfront.bfapi.services.JobService;
 
-@Profile("test")
 @Configuration
 public class MockApiConfig extends BfApiConfig {
 	@Bean
+	@Primary
 	public Environment getEnvironmentConfiguration() throws MalformedURLException  {
 		URL url = new URL("http://localhost:99999");
 		return new Environment() {
@@ -26,6 +26,7 @@ public class MockApiConfig extends BfApiConfig {
 	}
 	
 	@Bean
+	@Primary
 	public DataSource getDataSource() {
 		DataSource src = new EmbeddedDatabaseBuilder().build();
 		System.err.println("Mock data source: " + src.toString());
@@ -33,6 +34,7 @@ public class MockApiConfig extends BfApiConfig {
 	}
 	
 	@Bean
+	@Primary
 	public JobService getJobService() {
 		return null;
 	}
