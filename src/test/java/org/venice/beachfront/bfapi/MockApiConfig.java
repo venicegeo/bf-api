bb3e44c69d4a57b2085ca694b881c257c56e93ba
Filip Sufitchi <filips@imagemattersllc.com>
2017-10-18 13:21:02 -0400

    Print db details to debug pipeline db connection issues

--- src/test/java/org/venice/beachfront/bfapi/MockApiConfig.java
+++ src/test/java/org/venice/beachfront/bfapi/MockApiConfig.java
@@ -8,7 +8,6 @@ import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.venice.beachfront.bfapi.model.Environment;
