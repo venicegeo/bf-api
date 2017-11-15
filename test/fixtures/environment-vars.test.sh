41e2c1c2af10359ec346af89e8243cf6874ab61b
Patrick D <patrick.doody@rgi-corp.com>
2017-11-15 10:40:41 -0500

    Add Missing Env Var

--- test/fixtures/environment-vars.test.sh
+++ test/fixtures/environment-vars.test.sh
@@ -2,6 +2,7 @@ MUTE_LOGS=1
 GEOAXIS='test-geoaxis.test.localdomain'
 GEOAXIS_CLIENT_ID='test-geoaxis-client-id'
 GEOAXIS_SECRET='test-geoaxis-secret'
+GEOAXIS_AUTH='test-geoaxis-auth'
 DOMAIN='test.localdomain'
 PIAZZA_API_KEY='test-piazza-api-key'
 VCAP_SERVICES='{ "user-provided": [ { "name": "pz-postgres", "credentials": { "db_host": "test-hostname", "username": "test-username", "password": "test-password", "db_name": "test-database", "db_port": "test-port" } }, { "name": "pz-geoserver", "credentials": { "boundless_geoserver_url": "http://vcap-geoserver.test.localdomain/geoserver/index.html", "boundless_geoserver_username": "test-username", "boundless_geoserver_password": "test-password" } } ] }'
