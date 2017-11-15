bb2e0db76daeb5ee7c4075cd2c1a5ebdfcec0606
johnmcmahon <John.McMahon@digitalglobe.com>
2017-11-15 11:21:06 -0500

    Add geoaxis auth var

--- scripts/create-development-environment.sh
+++ scripts/create-development-environment.sh
@@ -26,6 +26,7 @@ create_environment_vars() {
     echo -e "\n$FUNCNAME: Creating $filepath"
     cat <<'EOT' | sed -E 's/^        //' > $filepath
         export GEOAXIS=
+        export GEOAXIS_AUTH=
         export GEOAXIS_CLIENT_ID=
         export GEOAXIS_SECRET=
         export DOMAIN=
