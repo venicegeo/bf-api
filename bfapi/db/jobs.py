76374f55fe0ba05418b9f19553c72706676981dc
Patrick D <patrick.doody@rgi-corp.com>
2017-12-21 10:08:33 -0500

    FIx SQL Syntax

--- bfapi/db/jobs.py
+++ bfapi/db/jobs.py
@@ -280,7 +280,7 @@ def select_for_existing_jobs(
     log = logging.getLogger(__name__)
     log.info('Db select jobs for scene and algorithm', action='database select record')
     query = """
-        SELECT j.job_id, j.algorithm_name, j.algorithm_version, j.created_by, j.created_on, j.name, j.scene_id, j.status, j.tide, j.tide_min_24h, j.tide_max_24h, j.compute_mask
+        SELECT j.job_id, j.algorithm_name, j.algorithm_version, j.created_by, j.created_on, j.name, j.scene_id, j.status, j.tide, j.tide_min_24h, j.tide_max_24h, j.compute_mask,
                ST_AsGeoJSON(s.geometry) AS geometry, s.sensor_name, s.captured_on
           FROM __beachfront__job j
                LEFT OUTER JOIN __beachfront__scene s ON (s.scene_id = j.scene_id)
