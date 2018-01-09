d11054a9c6678af9773ea8435050d1c9ff902160
Russell Orf <russellorf@datamentors.com>
2018-01-09 16:12:45 -0500

    orfr: adding CORS filter response headers

--- src/main/java/org/venice/beachfront/bfapi/BfApiConfig.java
+++ src/main/java/org/venice/beachfront/bfapi/BfApiConfig.java
@@ -3,6 +3,8 @@ package org.venice.beachfront.bfapi;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import java.net.MalformedURLException;
@@ -13,6 +15,9 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 import org.venice.beachfront.bfapi.model.Environment;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.joda.JodaModule;
@@ -72,5 +77,22 @@ public class BfApiConfig {
 				.username(vcapUsername)
 				.password(vcapPassword)
 				.build();
+	}
+
+	@Configuration
+	protected static class AddCorsHeaders extends WebMvcConfigurerAdapter {
+		@Override
+		public void addInterceptors(InterceptorRegistry registry) {
+			registry.addInterceptor(new HandlerInterceptorAdapter() {
+				@Override
+				public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+					response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+					response.setHeader("Access-Control-Allow-Origin", "*");
+					response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+					response.setHeader("Access-Control-Max-Age", "36000");
+					return true;
+				}
+			});
+		}
 	}
 }
