7ebbd64e419e760dcefa17fecd41b863535c9340
Patrick D <patrick.doody@rgi-corp.com>
2018-02-01 13:17:47 -0500

    Remove deprecated CI Folder

--- ci/cf_push_int.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-export GEOAXIS_DOMAIN=gxisaccess.gxaccess.com
-export PIAZZA_URL=https://piazza.int.geointservices.io/v2/key
-export MANIFEST_FILENAME=manifest.int.yml
-
-./ci/_cf_push.sh
--- ci/cf_push_stage.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-export GEOAXIS_DOMAIN=gxisaccess.gxaccess.com
-export PIAZZA_URL=https://piazza.stage.geointservices.io/v2/key
-export MANIFEST_FILENAME=manifest.stage.yml
-
-./ci/_cf_push.sh
