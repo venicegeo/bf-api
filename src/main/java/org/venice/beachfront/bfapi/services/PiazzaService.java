2b448989ab9a4c19950e6e522f06875cd64be0df
Patrick D <patrick.doody@rgi-corp.com>
2018-02-07 13:30:53 -0500

    Executing Jobs

--- src/main/java/org/venice/beachfront/bfapi/services/PiazzaService.java
+++ src/main/java/org/venice/beachfront/bfapi/services/PiazzaService.java
@@ -1,12 +1,19 @@
 package org.venice.beachfront.bfapi.services;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.URI;
 import java.nio.charset.Charset;
 import java.util.List;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,12 +41,23 @@ public class PiazzaService {
 	 * @param userId
 	 *            The ID of the creating user for this job
 	 * @return The Piazza Job ID
+	 * @throws Exception
 	 */
-	public String execute(String serviceId, String cliCommand, List<String> fileNames, List<String> fileUrls, String userId) {
+	public String execute(String serviceId, String cliCommand, List<String> fileNames, List<String> fileUrls, String userId)
+			throws Exception {
 		String piazzaJobUrl = String.format("%s/job", PIAZZA_URL);
 		HttpHeaders headers = createPiazzaHeaders(PIAZZA_API_KEY);
+		// Structure the Job Request
+		String requestJson = String.format(loadJobRequestJson(), serviceId, cliCommand, String.join(", ", fileNames),
+				String.join(", ", fileUrls), userId);
+		HttpEntity<String> request = new HttpEntity<>(requestJson, headers);
 
-		// Structure the payload that Piazza Expects
+		// Execute the Request
+		ResponseEntity<String> response = restTemplate.exchange(new URI(piazzaJobUrl), HttpMethod.POST, request, String.class);
+
+		// Parse the Response
+
+		// Return the Job ID
 
 		return null;
 	}
@@ -60,5 +78,30 @@ public class PiazzaService {
 				set("Authorization", authHeader);
 			}
 		};
+	}
+
+	/**
+	 * Gets the JSON for a Piazza job request, with string format parameters that must be filled in
+	 * 
+	 * @return JSON job request template
+	 */
+	private String loadJobRequestJson() throws Exception {
+		// Create the JSON Payload for the Layer request to GeoServer
+		ClassLoader classLoader = getClass().getClassLoader();
+		InputStream jsonStream = null;
+		String jsonString = null;
+		try {
+			jsonStream = classLoader.getResourceAsStream(String.format("%s/%s", "piazza", File.separator, "execute.json"));
+			jsonString = IOUtils.toString(jsonStream);
+		} finally {
+			try {
+				if (jsonStream != null) {
+					jsonStream.close();
+				}
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		}
+		return jsonString;
 	}
 }
