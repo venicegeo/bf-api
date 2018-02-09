c0d55ec100e5d459ccbbff371a7ebecf58d950ce
Patrick D <patrick.doody@rgi-corp.com>
2018-02-09 07:51:45 -0500

    Redundant Jobs Check

--- src/main/java/org/venice/beachfront/bfapi/database/dao/JobDao.java
+++ src/main/java/org/venice/beachfront/bfapi/database/dao/JobDao.java
@@ -10,5 +10,8 @@ import org.venice.beachfront.bfapi.model.Job;
 public interface JobDao extends CrudRepository<Job, Long> {
 	Job findByJobId(String jobId);
 
-    List<Job> findByAlgorithmIdAndSceneId(String algorithmId, String sceneId);
+	List<Job> findByAlgorithmIdAndSceneId(String algorithmId, String sceneId);
+
+	List<Job> findBySceneIdAndAlgorithmIdAndAlgorithmVersionAndComputeMaskAndStatus(String sceneId, String algorithmId,
+			String algorithmVersion, Boolean computeMask, String status);
 }
