a5e0658c8db5bcec68fa919163b929921a87cd93
Patrick D <patrick.doody@rgi-corp.com>
2018-03-06 09:33:27 -0500

    Fixing Unit Tests

--- src/test/java/org/venice/beachfront/bfapi/services/SceneServiceTests.java
+++ src/test/java/org/venice/beachfront/bfapi/services/SceneServiceTests.java
@@ -98,6 +98,7 @@ public class SceneServiceTests {
 		bands.set("nir", JsonNodeFactory.instance.textNode("NIR_URL"));
 		bands.set("swir1", JsonNodeFactory.instance.textNode("SWIR1_URL"));
 		JsonNode properties = JsonNodeFactory.instance.objectNode().set("bands", bands);
+		((ObjectNode) properties).put("location", "LOCATION_URL");
 		JsonNode rawJson = JsonNodeFactory.instance.objectNode().set("properties", properties);
 
 		Scene scene = new Scene();
@@ -107,11 +108,11 @@ public class SceneServiceTests {
 
 		scene.setSceneId("rapideye:abcd");
 		inputURLs = this.sceneService.getSceneInputURLs(scene);
-		assertEquals(Arrays.asList("SCENE_URL"), inputURLs);
+		assertEquals(Arrays.asList("LOCATION_URL"), inputURLs);
 
 		scene.setSceneId("planetscope:abcd");
 		inputURLs = this.sceneService.getSceneInputURLs(scene);
-		assertEquals(Arrays.asList("SCENE_URL"), inputURLs);
+		assertEquals(Arrays.asList("LOCATION_URL"), inputURLs);
 
 		scene.setSceneId("landsat:abcd");
 		inputURLs = this.sceneService.getSceneInputURLs(scene);
@@ -278,7 +279,7 @@ public class SceneServiceTests {
 		assertEquals(3, scene.getResolution());
 		assertEquals(DateTime.parse("2018-02-26T07:28:08Z"), scene.getCaptureTime());
 		assertEquals("mockSensorName", scene.getSensorName());
-		assertEquals("https://mock-uri.localdomain/foo/tile.avi", scene.getUri());
+		assertEquals("https://bf-ia-broker-test.localdomain:443/planet/rapideye/EXTERNAL_ID", scene.getUri());
 		assertEquals(Scene.STATUS_INACTIVE, scene.getStatus());
 		assertEquals(0.5, scene.getTide(), 0.0001);
 		assertEquals(0.1, scene.getTideMin24H(), 0.0001);
@@ -309,7 +310,7 @@ public class SceneServiceTests {
 		assertEquals(3, scene.getResolution());
 		assertEquals(DateTime.parse("2018-02-26T07:28:08Z"), scene.getCaptureTime());
 		assertEquals("mockSensorName", scene.getSensorName());
-		assertEquals("https://mock-uri.localdomain/foo/tile.avi", scene.getUri());
+		assertEquals("https://bf-ia-broker-test.localdomain:443/planet/landsat/EXTERNAL_ID", scene.getUri());
 		assertEquals(Scene.STATUS_ACTIVE, scene.getStatus()); // This is the big difference from rapideye; landsat
 																// should always be active
 		assertNull(scene.getTide());
@@ -323,16 +324,16 @@ public class SceneServiceTests {
 		JsonNode responseJson = new ObjectMapper().readTree(
 				getClass().getClassLoader().getResourceAsStream(String.format("%s%s%s", "scene", File.separator, "getLandsatScene.json")));
 		Mockito.when(this.restTemplate.getForEntity(Mockito.any(), Mockito.eq(JsonNode.class)))
-		.then(new Answer<ResponseEntity<JsonNode>>() {
-			public ResponseEntity<JsonNode> answer(InvocationOnMock invocation) {
-				Object[] args = invocation.getArguments();
-				assertEquals(URI.class, args[0].getClass());
-				assertEquals(
-						"https://bf-ia-broker-test.localdomain:443/planet/landsat/EXTERNAL_ID?PL_API_KEY=api-abc-123&tides=false",
-						((URI) args[0]).toString());
-				return new ResponseEntity<JsonNode>(responseJson, HttpStatus.OK);
-			}
-		});
+				.then(new Answer<ResponseEntity<JsonNode>>() {
+					public ResponseEntity<JsonNode> answer(InvocationOnMock invocation) {
+						Object[] args = invocation.getArguments();
+						assertEquals(URI.class, args[0].getClass());
+						assertEquals(
+								"https://bf-ia-broker-test.localdomain:443/planet/landsat/EXTERNAL_ID?PL_API_KEY=api-abc-123&tides=false",
+								((URI) args[0]).toString());
+						return new ResponseEntity<JsonNode>(responseJson, HttpStatus.OK);
+					}
+				});
 		// Test
 		Scene scene = this.sceneService.getScene("landsat:EXTERNAL_ID", "api-abc-123", false);
 		// Verify
