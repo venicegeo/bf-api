f5614872b3cc4f491d1d43bc2256e0d52f75627d
Filip Sufitchi <filips@imagemattersllc.com>
2018-03-06 14:05:16 -0500

    Add handling for RuntimeException that wraps UserException

--- src/main/java/org/venice/beachfront/bfapi/controllers/UserExceptionHandler.java
+++ src/main/java/org/venice/beachfront/bfapi/controllers/UserExceptionHandler.java
@@ -1,6 +1,11 @@
 package org.venice.beachfront.bfapi.controllers;
 
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -22,5 +27,22 @@ public class UserExceptionHandler {
 		return ResponseEntity.status(ex.getRecommendedStatusCode())
 				.contentType(MediaType.TEXT_PLAIN)
 				.body(ex.getMessage());
+	}
+	
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+		if (ex.getCause() != null && ex.getCause().getClass() == UserException.class) {
+			return this.handleUserException((UserException)ex.getCause());
+		}
+		String logMessage = String.format("[500] Unknown runtime error -- %s", ex.getMessage());
+		this.logger.log(logMessage, Severity.ERROR);
+		
+		StringWriter sw = new StringWriter();
+		ex.printStackTrace(new PrintWriter(sw));
+		this.logger.log(sw.toString(), Severity.ERROR);
+		
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+			.contentType(MediaType.TEXT_PLAIN)
+			.body(ex.getMessage());
 	}
 }
