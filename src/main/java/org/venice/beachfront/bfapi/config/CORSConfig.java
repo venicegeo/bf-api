6f8f06168a329e80b3f16a9bd403c9acd7e49448
Russell Orf <russellorf@datamentors.com>
2018-04-10 16:40:06 -0400

    orfr: cleaning up the rebase

--- src/main/java/org/venice/beachfront/bfapi/config/CORSConfig.java
+++ src/main/java/org/venice/beachfront/bfapi/config/CORSConfig.java
@@ -3,6 +3,7 @@ package org.venice.beachfront.bfapi.config;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,13 +14,17 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  */
 @Configuration
 public class CORSConfig extends WebMvcConfigurerAdapter {
+
+	@Value("${DOMAIN}")
+	private String domain;
+
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
 		registry.addInterceptor(new HandlerInterceptorAdapter() {
 			@Override
 			public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-				response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
-				response.setHeader("Access-Control-Allow-Origin", "*");
+				response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, X-Requested-With");
+				response.setHeader("Access-Control-Allow-Origin", "https://beachfront." + domain);
 				response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 				response.setHeader("Access-Control-Max-Age", "36000");
 				return true;
