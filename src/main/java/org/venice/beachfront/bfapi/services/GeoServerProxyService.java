1b7f800753e3c553d9c7877405b55d529b3a7137
Patrick D <patrick.doody@rgi-corp.com>
2018-05-14 08:58:49 -0400

    Add GeoServer Proxy Controller

---
+++ src/main/java/org/venice/beachfront/bfapi/services/GeoServerProxyService.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2018, Radiant Solutions, Inc.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+package org.venice.beachfront.bfapi.services;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.client.RestTemplate;
+import org.venice.beachfront.bfapi.geoserver.AuthHeaders;
+import org.venice.beachfront.bfapi.geoserver.GeoserverEnvironment;
+import org.venice.beachfront.bfapi.model.exception.UserException;
+
+import model.logger.Severity;
+import util.PiazzaLogger;
+
+/**
+ * Service that proxies requests to the GeoServer instance
+ */
+@Service
+public class GeoServerProxyService {
+	@Autowired
+	private RestTemplate restTemplate;
+	@Autowired
+	private PiazzaLogger piazzaLogger;
+	@Autowired
+	private GeoserverEnvironment geoserverEnvironment;
+	@Autowired
+	private AuthHeaders authHeaders;
+
+	public ResponseEntity<byte[]> proxyRequest(HttpServletRequest request)
+			throws MalformedURLException, IOException, URISyntaxException, UserException {
+		String requestPath = request.getRequestURI();
+		// Form the complete URI by piecing together the GeoServer URL with the API proxy request parameters
+		URL geoserverUrl = new URL(geoserverEnvironment.getGeoServerBaseUrl());
+		URI requestUri = new URI(geoserverUrl.getProtocol(), null, geoserverUrl.getHost(), geoserverUrl.getPort(), requestPath,
+				request.getQueryString(), null);
+		piazzaLogger.log(String.format("Proxying request to GET GeoServer at URI %s", requestUri.toString()), Severity.INFORMATIONAL);
+		try {
+			HttpEntity<String> requestHeaders = new HttpEntity<>(authHeaders.get());
+			ResponseEntity<byte[]> response = restTemplate.exchange(requestUri, HttpMethod.GET, requestHeaders, byte[].class);
+			return response;
+		} catch (HttpClientErrorException | HttpServerErrorException exception) {
+			piazzaLogger.log(String.format("Received GeoServer error response, code=%d, length=%d, for URI %s",
+					exception.getStatusCode().value(), exception.getResponseBodyAsString().length(), requestUri.toString()),
+					Severity.ERROR);
+			if (exception.getStatusCode().equals(HttpStatus.UNAUTHORIZED) || exception.getStatusCode().equals(HttpStatus.FORBIDDEN)) {
+				throw new UserException("Bad Authentication with GeoServer", exception, exception.getResponseBodyAsString(),
+						HttpStatus.BAD_REQUEST);
+			}
+			throw new UserException("Upstream GeoServer error", exception, exception.getResponseBodyAsString(), exception.getStatusCode());
+		}
+	}
+}
