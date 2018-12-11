cd683b7ae94730f4f80a220b6426e89ca7d7d019
Patrick D <patrick.doody@rgi-corp.com>
2018-12-11 12:18:26 -0500

    BEAC-146 Remove conflicting paths

--- src/main/java/org/venice/beachfront/bfapi/controllers/HealthCheckController.java
+++ src/main/java/org/venice/beachfront/bfapi/controllers/HealthCheckController.java
@@ -109,7 +109,7 @@ public class HealthCheckController {
 	@ResponseBody
 	public byte[] getPlanetApplication() throws UserException {
 		try {
-			return IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream("application" + File.separator + "planet.xlsx"));
+			return IOUtils.toByteArray(getClass().getClassLoader().getResourceAsStream("applications" + File.separator + "planet.xlsx"));
 		} catch (Exception exception) {
 			throw new UserException("Error downloading application.", exception, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
